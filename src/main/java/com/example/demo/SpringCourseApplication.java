d1effd99a579f820f67a7b2b221ea4c44583df00
EddWilly <lawiletxd@live.com>
2021-04-12 11:05:55 -0300

    First commit

--- src/main/java/com/example/demo/SpringCourseApplication.java
+++ src/main/java/com/example/demo/SpringCourseApplication.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class SpringCourseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringCourseApplication.class, args);
+
+		System.out.println("Teto");
 	}
+
 
 }
